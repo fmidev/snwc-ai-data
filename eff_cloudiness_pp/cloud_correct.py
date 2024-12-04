65057250a57811115a28e4259b05e51c8a224222
Leila Hieta <konkola@gpu-dev.fmi.fi>
2024-12-04 14:46:21 +0200

    fix chunk size

--- eff_cloudiness_pp/cloud_correct.py
+++ eff_cloudiness_pp/cloud_correct.py
@@ -23,18 +23,14 @@ def parse_command_line():
     args = parser.parse_args()
     return args
 
-
-
 def read_grid(args):
     """Top function to read "all" gridded data"""
 
     lons, lats, vals, analysistime, forecasttime = read_grib(args.rh, True)
-    _, _, nl, _, _ = read_grib(args.nl, False) #, "/home/users/hietal/.s3cfg-arcus")
-    _, _, nm, _, _ = read_grib(args.nm, False, "/home/users/hietal/.s3cfg-arcus")
-    _, _, nh, _, _ = read_grib(args.nh, False) #, "/home/users/hietal/.s3cfg-arcus")
+    _, _, nl, _, _ = read_grib(args.nl, False) 
+    _, _, nm, _, _ = read_grib(args.nm, False)
+    _, _, nh, _, _ = read_grib(args.nh, False)
 
-    #if args.parameter == "r":
-    #    vals = vals * 100
     return lons, lats, vals, analysistime, forecasttime, nl, nm, nh
 
 def read_zarr(filen):
@@ -88,12 +84,12 @@ def main():
     x = template["x"].values
     # read clearsky data
     cs = xr.open_zarr("clearsky.zarr")
-    # read nwcsaf data (effc, cttp, cmqc)
+    # read nwcsaf data (effc, cttp), might have missing times!
     nwcsaf = read_zarr("s3://nwcsaf-archive-data/nwcsaf.zarr")
+    #print("DS output",nwcsaf)
     # s3 template
     s3_rh = "s3://meps-ai-data/meps/{year}/{month}/{dd}/{timedate}_rcorr_heightAboveGround_2.grib2"
     
-    # loop over the days
     cloud = []
     for day in date_series:
         print(day)
@@ -118,88 +114,99 @@ def main():
         for hour in range(0, 24):
             # create time for hourly data
             timehour = day + timedelta(hours=hour)
-            nwcsaf1 = nwcsaf.sel(time=timehour)
-            effc_tmp = nwcsaf1["effective_cloudiness"].values * 100
-            # replace na's in effc_tmp with 0
-            effc_tmp[np.isnan(effc_tmp)] = 0
-            # copy effc_tmp field for plotting
-            effc_copy = effc_tmp.copy()
-            cttp_tmp = nwcsaf1["cloudtop_temperature"].values - 273.15
-            #plot_single_border(effc_tmp, lats, lons, "effc", timehour, 0, 100, 60, 70, 20, 33) 
-            #plot_single_field(cttp_tmp, "cttp", timehour, template, -80, 5)
-            nh_tmp = nh[hour] * 100
-            nm_tmp = nm[hour] * 100
-            nl_tmp = nl[hour] * 100
-            rh_tmp = rh[hour] * 100
-            avg1 = uniform_filter(effc_tmp, size=3, mode='reflect')#, cval=np.nan)
-            #plot_single_field(avg1, "avg1", timehour, template, 0, 100)
-            avg2 = uniform_filter(effc_tmp, size=7, mode='reflect') #, cval=np.nan)
-            #plot_single_field(avg2, "avg2", timehour, template, 0, 100)
-            if (day.month >= 10 or day.month <= 3): # correction for wintertime 
-                # create cloudmask based on cttp
-                effc_tmp[(np.isfinite(cttp_tmp)) & (effc_tmp == 0)] = 80
-                # average out the small gaps in data
-                effc_tmp[(effc_tmp <= 90) & (avg1 > 65)] = 90
-                effc_tmp[(effc_tmp <= 90) & (avg2 > 55)] = 90
-                # reduce cloud cover if just high level clouds
-                # if (effc <10  && rh >= 86 and (nl > 80 or cmqc == 24) {effc = 60 }
-                effc_tmp[(effc_tmp < 10) & (rh_tmp >= 86) & (nl_tmp > 80)] #or cmqc_tmp == 24)] = 60
-                # if (effc <=60  && rh >= 98 and (nl > 20 or cmqc == 24 ) {pilvi = 80 }  
-                effc_tmp[(effc_tmp <= 60) & (rh_tmp >= 98) & (nl_tmp > 20)] #or cmqc_tmp == 24)] = 80
-            elif (day.month >= 4 or day.month <= 9): # summer correction
-                # radiation parameters: short wave and clear sky 
-                sr1 = sr.sel(time=slice(xa_day, xa_day))
-                cs1 = cs.sel(time=slice(cs_day, cs_day))
-                sr_tmp1 = sr1["ssrd"].values
-                cs_tmp1 = cs1["clearsky"].values
-                sr_tmp = sr_tmp1[hour]
-                cs_tmp = cs_tmp1[hour]
-                # reduce if just high level clouds
-                effc_tmp[(effc_tmp > 50) & (nh_tmp > 50) & (nl_tmp < 20) & (nm_tmp < 20)] = 50
-                # average out the small gaps in data
-                effc_tmp[(effc_tmp <= 80) & (avg2 > 60)] = 60
-                effc_tmp[(effc_tmp <= 70) & (avg1 > 50)] = 70
-                # reduce cloud cover if cumulus in midday
-                if (hour >= 7 and hour < 17): 
-                    sr_ratio = np.full_like(sr_tmp, np.nan)
-                    # avoid divide by zero and only apply if srad is above 100 W/m2
-                    valid_mask = sr_tmp >= 100
-                    sr_ratio[valid_mask] = sr_tmp[valid_mask] / cs_tmp[valid_mask]
-                    effc_tmp[(sr_ratio >= 0.65) & (effc_tmp > 80)] = 80
-                    effc_tmp[(sr_ratio >= 0.75) & (effc_tmp > 50)] = 50
-            #plot_single_border(effc_tmp, lats, lons, "effc_pp", timehour, 0, 100, 60, 70, 20, 33)        
-            #plot_single_field(effc_tmp, "effc_pp", timehour, template, 0, 100)   
-            plot_two_border(effc_copy, effc_tmp, lats, lons, "effc", "effc_pp", timehour, 0, 100, 60, 70, 20, 33)
-            # data start time "reference time"
-            ref_time = datetime.datetime(int(sY), int(sM), int(sD), 0, 0, 0)
-            # time of the hourly data
-            timedd = datetime.datetime(int(year), int(month), int(dd), hour, 0, 0)
-            hourly_ds = xr.Dataset(
-                {
-                    "nwcsaf_pp": (("time", "y", "x"), effc_tmp[np.newaxis, ...]),
-                },
-                coords={
-                    "time": [timedd],
-                    "y": y,
-                    "x": x,
-                },
+            try: # check if nwcsaf is missing for the time
+                nwcsaf1 = nwcsaf.sel(time=timehour)
+                nwcsaf1 = nwcsaf.sel(time=timehour)
+                effc_tmp = nwcsaf1["effective_cloudiness"].values * 100
+                # replace na's in effc_tmp with 0
+                effc_tmp[np.isnan(effc_tmp)] = 0
+                # copy effc_tmp field for plotting
+                effc_copy = effc_tmp.copy()
+                cttp_tmp = nwcsaf1["cloudtop_temperature"].values - 273.15
+                #plot_single_border(effc_tmp, lats, lons, "effc", timehour, 0, 100, 60, 70, 20, 33) 
+                #plot_single_field(cttp_tmp, "cttp", timehour, template, -80, 5)
+                nh_tmp = nh[hour] * 100
+                nm_tmp = nm[hour] * 100
+                nl_tmp = nl[hour] * 100
+                rh_tmp = rh[hour] * 100
+                avg1 = uniform_filter(effc_tmp, size=3, mode='reflect')#
+                #plot_single_field(avg1, "avg1", timehour, template, 0, 100)
+                avg2 = uniform_filter(effc_tmp, size=7, mode='reflect') #
+                #plot_single_field(avg2, "avg2", timehour, template, 0, 100)
+                if (day.month >= 10 or day.month <= 3): # correction for wintertime 
+                    # create cloudmask based on cttp
+                    effc_tmp[(np.isfinite(cttp_tmp)) & (effc_tmp == 0)] = 80
+                    # average out the small gaps in data
+                    effc_tmp[(effc_tmp <= 90) & (avg1 > 65)] = 90
+                    effc_tmp[(effc_tmp <= 90) & (avg2 > 55)] = 90
+                    # reduce cloud cover if just high level clouds
+                    # if (effc <10  && rh >= 86 and (nl > 80 or cmqc == 24) {effc = 60 }
+                    effc_tmp[(effc_tmp < 10) & (rh_tmp >= 86) & (nl_tmp > 80)] #or cmqc_tmp == 24)] = 60
+                    # if (effc <=60  && rh >= 98 and (nl > 20 or cmqc == 24 ) {pilvi = 80 }  
+                    effc_tmp[(effc_tmp <= 60) & (rh_tmp >= 98) & (nl_tmp > 20)] #or cmqc_tmp == 24)] = 80
+                elif (day.month >= 4 or day.month <= 9): # summer correction
+                    # radiation parameters: short wave and clear sky 
+                    sr1 = sr.sel(time=slice(xa_day, xa_day))
+                    cs1 = cs.sel(time=slice(cs_day, cs_day))
+                    sr_tmp1 = sr1["ssrd"].values
+                    cs_tmp1 = cs1["clearsky"].values
+                    sr_tmp = sr_tmp1[hour]
+                    cs_tmp = cs_tmp1[hour]
+                    # reduce if just high level clouds
+                    effc_tmp[(effc_tmp > 50) & (nh_tmp > 50) & (nl_tmp < 20) & (nm_tmp < 20)] = 50
+                    # average out the small gaps in data
+                    effc_tmp[(effc_tmp <= 80) & (avg2 > 60)] = 60
+                    effc_tmp[(effc_tmp <= 70) & (avg1 > 50)] = 70
+                    # reduce cloud cover if cumulus in midday
+                    if (hour >= 7 and hour < 17): 
+                        sr_ratio = np.full_like(sr_tmp, np.nan)
+                        # avoid divide by zero and only apply if srad is above 100 W/m2
+                        valid_mask = sr_tmp >= 100
+                        sr_ratio[valid_mask] = sr_tmp[valid_mask] / cs_tmp[valid_mask]
+                        effc_tmp[(sr_ratio >= 0.65) & (effc_tmp > 80)] = 80
+                        effc_tmp[(sr_ratio >= 0.75) & (effc_tmp > 50)] = 50
+                #plot_single_border(effc_tmp, lats, lons, "effc_pp", timehour, 0, 100, 60, 70, 20, 33)        
+                #plot_single_field(effc_tmp, "effc_pp", timehour, template, 0, 100)   
+                ###plot_two_border(effc_copy, effc_tmp, lats, lons, "effc", "effc_pp", timehour, 0, 100, 60, 70, 20, 33)
+                # data start time "reference time"
+                ref_time = datetime.datetime(int(sY), int(sM), int(sD), 0, 0, 0)
+                # time of the hourly data
+                timedd = datetime.datetime(int(year), int(month), int(dd), hour, 0, 0)
+                hourly_ds = xr.Dataset(
+                    {
+                        "tcc": (("time", "y", "x"), effc_tmp[np.newaxis, ...]),
+                    },
+                    coords={
+                        "time": [timedd],
+                        "y": y,
+                        "x": x,
+                    },
             
-            )
-            hourly_ds["time"].attrs = {}  # Clear any residual attributes
-            hourly_ds["time"].encoding = {
-            "units": f"hours since {ref_time.isoformat()}",
-            "calendar": "proleptic_gregorian",
-            "dtype": "int64",  
-            }
+                )
+                hourly_ds["time"].attrs = {}  # Clear any residual attributes
+                hourly_ds["time"].encoding = {
+                "units": f"hours since {ref_time.isoformat()}",
+                "calendar": "proleptic_gregorian",
+                "dtype": "int64",  
+                }
             
-            cloud.append(hourly_ds)
-     
+                cloud.append(hourly_ds)
+            except KeyError:
+                #print("No data for time:", timehour)
+                continue
     combined_ds = xr.concat(cloud, dim="time", combine_attrs="override")
     combined_ds["time"].encoding = {
     "units": f"hours since {ref_time.isoformat()}",
     "calendar": "proleptic_gregorian",
     "dtype": "int64",
     }
+    combined_ds["spatial_ref"] = xr.DataArray(
+    data=np.array(0),  # dummy data
+    attrs=nwcsaf["spatial_ref"].attrs  # Copy attributes from source
+    )
+    chunk_sizes = {"time": 1, "y": 1069, "x": 949}  # Adjust 'y' and 'x' to match your dimension names
+    combined_ds = combined_ds.chunk(chunk_sizes)
+    """
     combined_ds["spatial_ref"] = xr.DataArray(
     data=np.array(0),  
     attrs={
@@ -213,19 +220,11 @@ def main():
         "inverse_flattening": 298.257223563,
         },
     )
+    """
     # Add the grid_mapping attribute to variable
-    combined_ds["nwcsaf_pp"].attrs["grid_mapping"] = "spatial_ref"
-    #combined_ds["nwcsaf_pp"].attrs["Variable"] = "nwcsaf_pp"
-    print(combined_ds["spatial_ref"])
-    print(combined_ds["nwcsaf_pp"].attrs)
-    print("Time values after concatenation:", combined_ds["time"].values)
-    # print keys and attributes
-    #print("keys",combined_ds.keys())
-    #print("attributes:", combined_ds["time"].attrs)
-    #print("encoding:",combined_ds["time"].encoding)
-
+    combined_ds["tcc"].attrs["grid_mapping"] = "spatial_ref"
+    print(combined_ds)
     combined_ds.to_zarr(args.output, mode="w")
-   
     
 if __name__ == "__main__":
     main()
